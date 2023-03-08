fc1741a37053902359e55a24e23caffaf674c1a6
Hoc Lap Trinh <vtmbmt1125@gmail.com>
2023-03-08 18:54:13 +0700

    loopback4

---
+++ src/models/product.model.ts
@@ -0,0 +1,64 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Product extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  ID?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  price: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  description: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  image: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  to: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  backgroundImg: string;
+
+  @property({
+    type: 'object',
+    required: true,
+  })
+  rating: object;
+
+
+  constructor(data?: Partial<Product>) {
+    super(data);
+  }
+}
+
+export interface ProductRelations {
+  // describe navigational properties here
+}
+
+export type ProductWithRelations = Product & ProductRelations;
